93ecc82eef6d5e21d015a4bf2f2bf22952c374f7
Alba Martin <alba.martingz@gmail.com>
2026-03-16 11:23:19 +0100

    feat(user): add existsByEmail query method to UserRepository

--- src/main/java/io/github/lisalorita/ordermanagement/users/repository/UserRepository.java
+++ src/main/java/io/github/lisalorita/ordermanagement/users/repository/UserRepository.java
@@ -7,5 +7,5 @@ import io.github.lisalorita.ordermanagement.users.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  boolean existsByEmail(String email);
 }
